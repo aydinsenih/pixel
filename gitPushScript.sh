1c5813aee009bcf55ebf862842989499fd27ae0f
aydinsenih <aydinsenih@gmail.com>
2021-11-13 13:41:01 -0800

    test automation

--- gitPushScript.sh
+++ gitPushScript.sh
@@ -4,4 +4,4 @@ echo All files added to commit.
 read -p 'Type commit message: ' message
 git commit -m "$message"
 git push origin main
-echo your commit(s) pushed to main branch.
+echo your commit\(s\) pushed to main branch.
